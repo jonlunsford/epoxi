051e02bcbef1a0393e986a4c073b3089f9fe37e6
Jon Lunsford <jon@capturethecastle.net>
2018-04-26 14:44:57 -0700

    Add Mailer tests

--- test/epoxi/smtp/mailer_test.exs
+++ test/epoxi/smtp/mailer_test.exs
@@ -1,3 +1,27 @@
 defmodule Epoxi.SMTP.MailerTest do
   use ExUnit.Case
+
+  alias Epoxi.SMTP.Mailer
+
+  alias Epoxi.Test.Helpers
+  alias Epoxi.Test.Context
+
+  setup_all do
+    Mailman.TestServer.start
+    :ok
+  end
+
+  describe  "deliver" do
+    test "it succeeds" do
+      email = Helpers.email_to(["test@gmail.com"])
+
+      assert {:ok, _message} = Mailer.deliver(email, Context)
+    end
+
+    test "it handles long form addresses" do
+      email = Helpers.email_to(["Test Tester <test@gmail.com>"])
+
+      assert {:ok, _message} = Mailer.deliver(email, Context)
+    end
+  end
 end
