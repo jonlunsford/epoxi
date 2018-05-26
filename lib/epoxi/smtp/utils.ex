101dabbd2701a275506a01bb9c622b24f9aeabf0
Jon Lunsford <jon@capturethecastle.net>
2018-05-26 13:18:41 -0500

    Add Epoxi.SMTP.Utils.atomize_keys functions

--- lib/epoxi/smtp/utils.ex
+++ lib/epoxi/smtp/utils.ex
@@ -47,6 +47,33 @@ defmodule Epoxi.SMTP.Utils do
     :inet_res.lookup(to_charlist(domain), :in, :mx)
   end
 
+  @doc """
+  Convert map string keys to :atom keys
+  """
+  def atomize_keys(nil), do: nil
+
+  # Structs don't do enumerable and anyway the keys are already
+  # atoms
+  def atomize_keys(struct = %{__struct__: _}) do
+    struct
+  end
+
+  def atomize_keys(map = %{}) do
+    map
+    |> Enum.map(fn {k, v} -> {String.to_atom(k), atomize_keys(v)} end)
+    |> Enum.into(%{})
+  end
+
+  # Walk the list and atomize the keys of
+  # of any map members
+  def atomize_keys([head | rest]) do
+    [atomize_keys(head) | atomize_keys(rest)]
+  end
+
+  def atomize_keys(not_a_map) do
+    not_a_map
+  end
+
   defp load_ns() do
     case List.keyfind(:inet_db.get_rc(), :nameserver, 0) do
       nil ->
