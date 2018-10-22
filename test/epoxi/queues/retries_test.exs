ad64b8d45a59898a22d3ca0d6f10c78cb44867ab
Jon Lunsford <jon@capturethecastle.net>
2018-10-22 15:20:31 -0700

    Test retrying

--- test/epoxi/queues/retries_test.exs
+++ test/epoxi/queues/retries_test.exs
@@ -2,51 +2,62 @@ defmodule Epoxi.Queues.RetriesTest do
   use ExUnit.Case
 
   alias Epoxi.Queues.Retries
+  alias Epoxi.Test.Helpers
 
   setup do
-    inbox = start_supervised!({Retries, :queue.new})
+    retries = start_supervised!({Retries, :queue.new})
 
-    %{inbox: inbox}
+    %{retries: retries}
   end
 
   describe "enqueue" do
-    test "it enqueues events", %{inbox: inbox} do
-      assert {:ok, "enqueued"} = Retries.enqueue(inbox, %{message: "deliver", payload: %{email: "..."}})
+    test "it enqueues events", %{retries: retries} do
+      assert {:ok, "enqueued"} = Retries.enqueue(retries, %{message: "deliver", payload: %{email: "..."}})
     end
   end
 
   describe "dequeue" do
-    test "it returns an item from the queue", %{inbox: inbox} do
+    test "it returns an item from the queue", %{retries: retries} do
       payload = %{message: "out", payload: %{}}
-      Retries.enqueue(inbox, payload)
+      Retries.enqueue(retries, payload)
 
-      assert Retries.dequeue(inbox) == [payload]
+      assert Retries.dequeue(retries) == [payload]
     end
 
-    test "it returns items FIFO", %{inbox: inbox} do
+    test "it returns items FIFO", %{retries: retries} do
       payload_1 = %{message: "first", payload: %{}}
       payload_2 = %{message: "second", payload: %{}}
 
-      Retries.enqueue(inbox, payload_1)
-      Retries.enqueue(inbox, payload_2)
+      Retries.enqueue(retries, payload_1)
+      Retries.enqueue(retries, payload_2)
 
-      assert Retries.dequeue(inbox) == [payload_1]
+      assert Retries.dequeue(retries) == [payload_1]
     end
 
-    test "it replies with an empty message", %{inbox: inbox} do
-      assert {:ok, :empty} = Retries.dequeue(inbox)
+    test "it replies with an empty message", %{retries: retries} do
+      assert {:ok, :empty} = Retries.dequeue(retries)
     end
   end
 
   describe "queue_size" do
-    test "it returns the total queue size", %{inbox: inbox} do
+    test "it returns the total queue size", %{retries: retries} do
       payload_1 = %{message: "first", payload: %{}}
       payload_2 = %{message: "second", payload: %{}}
 
-      Retries.enqueue(inbox, payload_1)
-      Retries.enqueue(inbox, payload_2)
+      Retries.enqueue(retries, payload_1)
+      Retries.enqueue(retries, payload_2)
 
-      assert Retries.queue_size(inbox) == 2
+      assert Retries.queue_size(retries) == 2
+    end
+  end
+
+  describe "retry" do
+    test "it re-enqueues the failure to an available inbox", %{retries: retries} do
+      failed_attempt = Helpers.test_json_string()
+
+      Retries.enqueue(retries, failed_attempt)
+
+      assert Retries.retry(retries) == [failed_attempt]
     end
   end
 end
