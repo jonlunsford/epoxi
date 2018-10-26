438bea3b6751dedb860e4e2da400e87f3025d83c
Jon Lunsford <jon@capturethecastle.net>
2018-10-26 16:07:04 -0700

    Remove unused helper function

--- lib/epoxi/smtp/mailer.ex
+++ lib/epoxi/smtp/mailer.ex
@@ -11,24 +11,4 @@ defmodule Epoxi.SMTP.Mailer do
 
     Mailman.deliver(%{email | to: [to]}, context)
   end
-
-  @spec email_to(recipients :: List.t(String.t())) :: %Mailman.Email{}
-  def email_to(recipients) do
-    %Mailman.Email{
-      subject: "Hello!",
-      from: "test@localhost.com",
-      to: recipients,
-      data: [
-        name: "Yo"
-      ],
-      text: "Hello! <%= name %> These are Unicode: qżźół",
-      html: """
-            <html>
-            <body>
-             <b>Hello! <%= name %></b> These are Unicode: qżźół
-            </body>
-            </html>
-            """
-    }
-  end
 end
