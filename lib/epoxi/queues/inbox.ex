e77915cadc23a0d6e9a99bf42b68951591bcb2f4
Jon Lunsford <jon@capturethecastle.net>
2018-10-26 16:07:27 -0700

    Use list and forward events as they arrive

--- lib/epoxi/queues/inbox.ex
+++ lib/epoxi/queues/inbox.ex
@@ -6,59 +6,40 @@ defmodule Epoxi.Queues.Inbox do
   supplying a queue recovered from disk.
 
   Expected payload:
-    `"some JSON string"`
+    `some JSON string`
   """
 
-  use GenServer
+  use GenStage
 
-  def start_link(queue) do
-    GenServer.start_link(__MODULE__, queue)
+  def start_link(state) do
+    GenStage.start_link(__MODULE__, state, name: __MODULE__)
   end
 
   ## Public API
 
   def enqueue(pid, payload) do
-    GenServer.call(pid, {:enqueue, payload})
-  end
-
-  def dequeue(pid) do
-    GenServer.call(pid, :dequeue)
-  end
-
-  def drain(pid) do
-    GenServer.call(pid, :drain)
+    GenStage.cast(pid, {:enqueue, payload})
   end
 
   def queue_size(pid) do
-    GenServer.call(pid, :queue_size)
+    GenStage.call(pid, :queue_size)
   end
 
   ## Callbacks
 
-  def init(queue) do
-    {:ok, queue}
+  def init(state) do
+    {:producer, state}
   end
 
-  def handle_call({:enqueue, payload}, _from, queue) do
-    queue = :queue.in(payload, queue)
-    {:reply, {:ok, "enqueued"}, queue}
+  def handle_cast({:enqueue, payload}, state) when is_list(payload) do
+    {:noreply, payload, state}
   end
 
-  def handle_call(:drain, _from, queue) do
-    {new_queue, remaining_queue} = :queue.split(:queue.len(queue), queue)
-    {:reply, :queue.to_list(new_queue), remaining_queue}
+  def handle_cast({:enqueue, payload}, state), do: {:noreply, [payload], state}
+
+  def handle_call(:queue_size, _from, state) do
+    {:reply, Enum.count(state), state}
   end
 
-  def handle_call(:dequeue, _from, queue) do
-    case :queue.out(queue) do
-      {{:value, item}, new_queue} ->
-        {:reply, [item], new_queue}
-      {:empty, cur_queue} ->
-        {:reply, {:ok, :empty}, cur_queue}
-    end
-  end
-
-  def handle_call(:queue_size, _from, queue) do
-    {:reply, :queue.len(queue), queue}
-  end
+  def handle_demand(_, state), do: {:noreply, [], state}
 end
