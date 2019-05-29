bf641f136191c553048febc5b1f1c6d439044da7
Jon Lunsford <jon@capturethecastle.net>
2019-05-29 13:32:43 -0700

    Create helper to generate json payloads

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -1,57 +1,28 @@
 defmodule Epoxi.Test.Helpers do
   @moduledoc "Generic test helper functions"
 
-  @spec email_to(recipients :: List.t(String.t())) :: %Mailman.Email{}
-  def email_to(recipients) do
-    %Mailman.Email{
-      subject: "Hello!",
-      from: "test@localhost.com",
+  def gen_json_payload(batch_size) do
+    data = build_batch_data(batch_size)
+    recipients = Map.keys(data)
+
+    %{
+      from: "test@test.com",
       to: recipients,
-      data: [
-        name: "Yo"
-      ],
-      text: "Hello! <%= name %> These are Unicode: qżźół",
-      html: """
-            <html>
-            <body>
-             <b>Hello! <%= name %></b> These are Unicode: qżźół
-            </body>
-            </html>
-            """
+      subject: "Test Subject",
+      text: "Hello Text! <%= first_name %> <%= last_name %>",
+      html: "Hello HTML! <%= first_name %> <%= last_name %>",
+      data: data
     }
+    |> Poison.encode!
   end
 
-  def multi_recipients_json do
-    """
-    {
-      "from": "test@test.com",
-      "to": ["test1@test.com", "test2@test.com", "test3@test.com"],
-      "subject": "Test Subject",
-      "text": "Hello Text! <%= first_name %> <%= last_name %>",
-      "html": "Hello HTML! <%= first_name %> <%= last_name %>",
-      "data": {
-        "test1@test.com": { "first_name": "test1first", "last_name": "test1last" },
-        "test2@test.com": { "first_name": "test2first", "last_name": "test2last" },
-        "test3@test.com": { "first_name": "test3first", "last_name": "test3last" }
-      }
-    }
-    """
-    |> String.trim()
+  def build_batch_data(size) do
+    (1..size)
+    |> Enum.reduce(%{}, &generate_data/2)
   end
 
-  def test_json_string() do
-    """
-    {
-      "from": "test@test.com",
-      "to": ["test1@test.com"],
-      "subject": "Test Subject",
-      "text": "Hello Text! <%= first_name %> <%= last_name %>",
-      "html": "Hello HTML! <%= first_name %> <%= last_name %>",
-      "data": {
-        "test1@test.com": { "first_name": "test1first", "last_name": "test1last" }
-      }
-    }
-    """
-    |> String.trim()
+  def generate_data(num, map) do
+    map
+    |> Map.put("test#{num}@test.com", %{first_name: "test#{num}first", last_name: "test#{num}last"})
   end
 end
