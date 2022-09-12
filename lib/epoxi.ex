c4da7e3140b7151a07135c60d73e20e746094441
Jon Lunsford <jon@capturethecastle.net>
2022-09-12 14:14:50 -0700

    Start public API

--- lib/epoxi.ex
+++ lib/epoxi.ex
@@ -1,18 +1,39 @@
 defmodule Epoxi do
   @moduledoc """
-  Documentation for Epoxi.
+  Epoxi - A complete mail server
   """
 
-  @doc """
-  Hello world.
+  alias Epoxi.Render
+  alias Epoxi.Parsing
+  alias Epoxi.Utils
 
-  ## Examples
+  defprotocol Adapter do
+    @moduledoc "Protocol for sending adapters to implement"
+    def send_blocking(context, email, message)
+    #def deliver(config, email, message)
+  end
 
-      iex> Epoxi.hello
-      :world
+  defprotocol Composer do
+    @moduledoc "Protocol for composers to impliment"
+    def compile_part(config, mode, email)
+  end
 
-  """
-  def hello do
-    :world
+  def send_blocking(email, context) do
+    # TODO: pre-cache mx records for popular domains
+    {_priority, relay} =
+      Parsing.get_hostname(email.to)
+      |> Utils.mx_lookup()
+      |> List.first()
+
+    message = Render.encode(email)
+    config = %{context.config | relay: relay}
+
+    case Adapter.send_blocking(config, email, message) do
+      {:error, reason} ->
+        IO.inspect(reason, label: :error_sending)
+
+      {:ok, response} ->
+        IO.inspect(response, label: :success_sending)
+    end
   end
 end
