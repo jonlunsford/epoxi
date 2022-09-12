d873aad351ace4d4d4b7c527a25ff0f5f405116a
Jon Lunsford <jon@capturethecastle.net>
2022-09-12 14:15:21 -0700

    Add Email.t()

--- lib/epoxi/email.ex
+++ lib/epoxi/email.ex
@@ -8,21 +8,38 @@ defmodule Epoxi.Email do
   defstruct subject: "",
             from: "",
             reply_to: "",
-            to: "",
+            to: [],
             cc: [],
             bcc: [],
             attachments: [],
             data: %{},
-            html: nil,
-            text: nil,
+            html: "",
+            text: "",
             delivery: nil,
-            content_type: nil
+            content_type: nil,
+            headers: %{}
 
-  def put_content_type(%Email{html: h, text: nil} = email) when is_bitstring(h) do
+  @type t :: %__MODULE__{
+          subject: String.t(),
+          from: String.t(),
+          reply_to: String.t(),
+          to: List.t(),
+          cc: List.t(),
+          bcc: List.t(),
+          attachments: List.t(),
+          data: Map.t(),
+          html: String.t(),
+          text: String.t(),
+          delivery: DateTime.t(),
+          content_type: String.t(),
+          headers: Map.t()
+        }
+
+  def put_content_type(%Email{html: h, text: ""} = email) when is_bitstring(h) do
     %{email | content_type: "text/html"}
   end
 
-  def put_content_type(%Email{html: nil, text: t} = email) when is_bitstring(t) do
+  def put_content_type(%Email{html: "", text: t} = email) when is_bitstring(t) do
     %{email | content_type: "text/plain"}
   end
 
