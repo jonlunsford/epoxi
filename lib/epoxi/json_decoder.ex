fa995cd77ee08decd4937b93c2477e6f9191d807
Jon Lunsford <jon@capturethecastle.net>
2022-09-26 22:12:42 -0700

    Use Jason instead of Poison

--- lib/epoxi/json_decoder.ex
+++ lib/epoxi/json_decoder.ex
@@ -6,17 +6,15 @@ defmodule Epoxi.JSONDecoder do
 
   require Logger
 
-  alias Epoxi.Utils
-
   def decode(json_string) when is_binary(json_string) do
-    case Poison.decode(json_string) do
+    case Jason.decode(json_string, [keys: :atoms]) do
       {:ok, result} -> cast_map_to_email_struct(result)
       {:error, _reason} -> {:error, "Failed to decode JSON."}
     end
   end
 
   defp cast_map_to_email_struct(payload) when is_map(payload) do
-    payload["to"]
+    payload[:to]
     |> Enum.map(&format_map_for_email(&1, payload))
     |> Enum.map(&cast_to_struct/1)
   end
@@ -28,8 +26,7 @@ defmodule Epoxi.JSONDecoder do
   end
 
   defp cast_to_struct(json_map) when is_map(json_map) do
-    map = Utils.atomize_keys(json_map)
-    map = update_in(map[:data], fn(m) -> Map.to_list(m) end)
+    map = update_in(json_map[:data], fn(m) -> Map.to_list(m) end)
     struct(Epoxi.Email, map)
   end
 end
