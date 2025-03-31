32c72e3cb6c663f4dbac0b69e9a1e9276d614b5e
Jon Lunsford <jon@liscio.me>
2025-03-31 16:41:55 -0700

    Add `Epoxi.Context.new/1`

--- lib/epoxi/context.ex
+++ lib/epoxi/context.ex
@@ -3,15 +3,28 @@ defmodule Epoxi.Context do
   Interface contexts must implement
   """
 
-  defstruct adapter: Epoxi.Adapters.SMTP,
+  defstruct client: Epoxi.SmtpClient,
             compiler: Epoxi.EExCompiler,
             config: %Epoxi.SmtpConfig{},
             socket: nil
 
   @type t :: %__MODULE__{
-          adapter: Epoxi.Adapter.t(),
+          client: module(),
           compiler: Epoxi.Compiler.t(),
           config: Epoxi.SmtpConfig.t(),
           socket: pid()
         }
+
+  @spec new(Keyword.t()) :: t()
+  def new(opts \\ []) do
+    smtp_config = Application.get_env(:epoxi, :smtp_config, %Epoxi.SmtpConfig{})
+    smtp_config = struct(Epoxi.SmtpConfig, smtp_config)
+
+    %__MODULE__{
+      client: Keyword.get(opts, :client, Epoxi.SmtpClient),
+      compiler: Keyword.get(opts, :compiler, Epoxi.EExCompiler),
+      config: smtp_config,
+      socket: Keyword.get(opts, :socket, nil)
+    }
+  end
 end
