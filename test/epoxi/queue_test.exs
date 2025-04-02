7cfac249f6095496ff9ac75dde5f7a369522540b
Jon Lunsford <jon@liscio.me>
2025-04-01 17:33:06 -0700

    WIP `Queue` API

---
+++ test/epoxi/queue_test.exs
@@ -0,0 +1,22 @@
+defmodule Epoxi.QueueTest do
+  use ExUnit.Case
+
+  alias Epoxi.{Email, Context, Queue}
+
+  setup do
+    context = Context.new()
+
+    {:ok, context: context}
+  end
+
+  test "enqueue and process an email", %{context: context} do
+    email = %Email{
+      from: "sender@example.com",
+      to: ["recipient@example.com"],
+      subject: "Test",
+      text: "Hello world"
+    }
+
+    {:ok, _id} = Queue.enqueue(email, context)
+  end
+end
