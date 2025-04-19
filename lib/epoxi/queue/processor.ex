5c347c58300f5d2a46d02face69be8e06869341d
Jon Lunsford <jon@liscio.me>
2025-04-19 10:31:33 -0700

    Nearly complete processing pipeline

--- lib/epoxi/queue/processor.ex
+++ lib/epoxi/queue/processor.ex
@@ -2,10 +2,10 @@ defmodule Epoxi.Queue.Processor do
   @moduledoc """
   Broadway pipeline for processing queued emails
   """
+  require Logger
 
   use Broadway
-
-  alias Epoxi.{Queue}
+  alias Epoxi.{SmtpConfig, SmtpClient, Parsing, Email}
 
   # @max_retries 3
   # 5min, 30min, 2hr
@@ -19,80 +19,90 @@ defmodule Epoxi.Queue.Processor do
       name: __MODULE__,
       producer: [
         module: {producer_module, producer_options},
-        transformer: {__MODULE__, :transform, []},
         concurrency: Keyword.get(opts, :concurrency, 1)
       ],
       processors: [
         default: [concurrency: 2]
       ],
       batchers: [
-        domain: [batch_size: 50, batch_timeout: 5000, concurrency: 10],
-        retry: [batch_size: 10, batch_timeout: 1000, concurrency: 2],
+        pending: [batch_size: 50, batch_timeout: 5000, concurrency: 10],
+        retrying: [batch_size: 10, batch_timeout: 1000, concurrency: 2],
         failed: [batch_size: 10, batch_timeout: 1000, concurrency: 2]
       ]
     )
   end
 
-  def transform(message, _opts) do
-    %Broadway.Message{
-      data: message,
-      acknowledger: {__MODULE__, :ack_id, message.id}
-    }
+  @impl true
+  def handle_message(_processor, %Broadway.Message{data: email} = message, _context) do
+    domain = Parsing.get_hostname(email.to)
+
+    message
+    |> Broadway.Message.put_batch_key(domain)
+    |> Broadway.Message.put_batcher(email.status)
   end
 
   @impl true
-  def handle_message(_processor, %Broadway.Message{data: data} = message, _context) do
-    domain = Epoxi.Parsing.get_hostname(data.email.to)
-    message = Broadway.Message.put_batch_key(message, domain)
+  def handle_batch(:pending, messages, batch_info, _context) do
+    deliver_batch(messages, batch_info)
+  end
 
-    case data.status do
-      :pending ->
+  @impl true
+  def handle_batch(:retrying, messages, batch_info, _context) do
+    {ready, not_ready} =
+      Enum.split_with(messages, fn message ->
+        Email.time_to_retry?(message.data)
+      end)
+
+    retried = deliver_batch(ready, batch_info)
+
+    re_enqueued =
+      Enum.map(not_ready, fn message ->
         message
-        |> Broadway.Message.put_batcher(:domain)
+        |> Broadway.Message.put_data(message.data)
+        |> Broadway.Message.failed(:pending)
+      end)
 
-      :retrying ->
-        if Queue.Message.time_to_retry?(data) do
-          message
-          |> Broadway.Message.put_batcher(:domain)
-        else
-          message
-          |> Broadway.Message.put_batcher(:retry)
-        end
-
-      :failed ->
-        message
-        |> Broadway.Message.put_batcher(:failed)
-    end
-
-    # Try to send the email
-    # result = SmtpClient.send_blocking(data.email, data.context)
-    #
-    # case result do
-    #   {:ok, receipt} ->
-    #     Broadway.Message.update_data(
-    #       message,
-    #       &Queue.Message.mark_delivered(&1, receipt)
-    #     )
-    #
-    #   {:error, reason} ->
-    #     Broadway.Message.update_data(
-    #       message,
-    #       &Queue.Message.mark_failed(&1, reason)
-    #     )
-    # end
+    retried ++ re_enqueued
   end
 
   @impl true
-  def handle_batch(:domain, messages, _batch_info, _context) do
-    # domain = batch_info.batch_key
-    # smtp_config = Epoxi.SmtpConfig.for_domain(domain)
-    messages
+  def handle_batch(:failed, messages, _batch_info, _context) do
+    Enum.map(messages, fn message ->
+      Broadway.Message.failed(message, :complete_failure)
+    end)
   end
 
   @impl true
-  def handle_batch(_batcher, messages, _batch_info, _context) do
-    messages
+  def handle_failed(messages, _context) do
+    {retry, dead} =
+      Enum.split_with(messages, fn message ->
+        Email.retrying?(message.data)
+      end)
+
+    OffBroadwayMemory.Buffer.push(:inbox, Enum.map(retry, & &1.data))
+
+    retry ++ dead
   end
 
-  def ack_id(_, _, _), do: :ok
+  defp deliver_batch(messages, batch_info) do
+    {:ok, socket} =
+      SmtpConfig.open_socket(
+        %SmtpConfig{port: 2525, relay: "localhost"},
+        batch_info.batch_key
+      )
+
+    Enum.map(messages, fn message ->
+      case SmtpClient.deliver_over_socket(message.data, socket) do
+        {:ok, email} ->
+          Broadway.Message.put_data(message, email)
+
+        {:error, email} ->
+          dbg(email)
+
+          message
+          |> Broadway.Message.put_data(email)
+          |> Broadway.Message.failed(:pending)
+      end
+    end)
+  end
 end
