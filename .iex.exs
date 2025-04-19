a72adbe2f2f7de151a64f000db7efc2c72330953
Jon Lunsford <jon@liscio.me>
2025-04-19 10:32:36 -0700

    Include test smtp server in iEX

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,5 @@
 Code.compile_file("test/support/test_helpers.ex")
+Code.compile_file("test/support/test_smtp_error_codes.ex")
+Code.compile_file("test/support/test_smtp_server.ex")
 
 alias Epoxi.Test.Helpers
