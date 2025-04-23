1a3f068d2d529dae2def1477e446ce6340a00674
Jon Lunsford <jon@capturethecastle.net>
2025-04-23 11:52:30 -0700

    Add Epoxi.Queue.Registry

--- lib/epoxi/application.ex
+++ lib/epoxi/application.ex
@@ -6,6 +6,7 @@ defmodule Epoxi.Application do
   def start(_type, _args) do
     children = [
       {Epoxi.Telemetry, []},
+      {Registry, keys: :unique, name: Epoxi.Queue.Registry},
       {OffBroadwayMemory.Buffer, name: :inbox},
       {Epoxi.Queue.Processor, [concurrency: 10]},
       {Bandit, Application.get_env(:epoxi, :endpoint_options)}
