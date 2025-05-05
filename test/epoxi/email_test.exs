c6615914020c83153678ebacd64a037fe1a9c842
Jon Lunsford <jon@capturethecastle.net>
2025-05-05 13:50:33 -0700

    Handle network failures when calculating retry

--- test/epoxi/email_test.exs
+++ test/epoxi/email_test.exs
@@ -38,11 +38,65 @@ defmodule Epoxi.EmailTest do
     end
   end
 
-  describe "time_to_retry/1" do
-    test "returns false when max retries have been reaached" do
+  describe "time_to_retry?/1" do
+    test "returns false when max retries have been reached" do
       email = %Email{retry_count: 5}
 
       refute Email.time_to_retry?(email)
+    end
+
+    test "returns false for failed status" do
+      email = %Email{status: :failed}
+
+      refute Email.time_to_retry?(email)
+    end
+
+    test "returns false for pending status" do
+      email = %Email{status: :pending}
+
+      refute Email.time_to_retry?(email)
+    end
+
+    test "returns true for retrying status with nil next_retry_at" do
+      email = %Email{status: :retrying, next_retry_at: nil}
+
+      assert Email.time_to_retry?(email)
+    end
+
+    test "returns true for retrying status with next_retry_at in the past" do
+      # 1 minute ago
+      past = DateTime.utc_now() |> DateTime.add(-60)
+      email = %Email{status: :retrying, next_retry_at: past}
+
+      assert Email.time_to_retry?(email)
+    end
+
+    test "returns false for retrying status with next_retry_at in the future" do
+      # 1 minute in future
+      future = DateTime.utc_now() |> DateTime.add(60)
+      email = %Email{status: :retrying, next_retry_at: future}
+
+      refute Email.time_to_retry?(email)
+    end
+  end
+
+  describe "retrying?/1" do
+    test "returns true for retrying status" do
+      email = %Email{status: :retrying}
+
+      assert Email.retrying?(email)
+    end
+
+    test "returns false for pending status" do
+      email = %Email{status: :pending}
+
+      refute Email.retrying?(email)
+    end
+
+    test "returns false for failed status" do
+      email = %Email{status: :failed}
+
+      refute Email.retrying?(email)
     end
   end
 end
