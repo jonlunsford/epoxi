99b67fe3ac46667e5119eb498c526045430642ca
Jon Lunsford <jon@capturethecastle.net>
2025-05-16 16:56:31 -0700

    Add TestCluster for distrubuted testing

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -5,7 +5,12 @@ support_dir
 |> Path.wildcard()
 |> Enum.map(&Code.compile_file/1)
 
-exclude = if Node.alive?(), do: [], else: [distributed: true]
+include = Keyword.get(ExUnit.configuration(), :include)
 
-ExUnit.start(exclude: exclude)
+if :distributed in include do
+  Epoxi.TestCluster.spawn([:"node1@127.0.0.1", :"node2@127.0.0.1"])
+end
+
 Epoxi.TestSmtpServer.start(2525)
+
+ExUnit.start(exclude: [distributed: true])
