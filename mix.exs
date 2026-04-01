419f08a2735163662cd19ba4377fac114a96c3dc
Jon Lunsford <jon@capturethecastle.net>
2026-03-31 18:57:44 -0700

    Update to Elixir 1.19, update deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Epoxi.MixProject do
     [
       app: :epoxi,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
